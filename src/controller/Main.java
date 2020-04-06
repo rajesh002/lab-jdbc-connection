aedaac11cdfdf74b65e952b2aebfbc7fdc4f3e47
rajesh002 <rajeshbtech16@gmail.com>
2020-04-06 16:00:04 +0530

    Connection eastblishing using properties is suceess

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,13 +1,29 @@
 package controller;
 
 import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Properties;
 
 import utility.ConnectionManager;
 
 public class Main {
-	public static void main(String args[]) {
+	public static void main(String args[]) throws Exception {
 		
-	// Fill your code
-		
+	ConnectionManager cm=new ConnectionManager();
+	Connection con=cm.getConnection();
+	
+	System.out.println("Using Getconnection method");
+	if(con!=null)
+		System.out.println("Connection Established");
+	else
+		System.out.println("Check your Connection");
+
+
+	Properties prop=cm.loadPropertiesFile();
+	System.out.println("Using Properties file");
+	if(prop!=null)
+		System.out.println("Connection Established");
+	else
+		System.out.println("Check your Connection");
 	}
 }
